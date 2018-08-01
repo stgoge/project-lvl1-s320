a7b5a63dea4f27f94f4bc7f3a09bdb9fe4af2dd6
Герман <stgoge@ya.ru>
2018-08-01 19:45:40 +0700

    v 0.2.0

---
+++ src/bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import { evenGame } from '../index';
+
+console.log('Welcome to the Brain Games!');
+evenGame();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import readlineSync from 'readline-sync';
 
-const askName = () => {
-  const actual = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${actual}!`);
+export const askName = () => {
+  const actualName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actualName}!`);
+  console.log();
+  return actualName;
 };
 
-export default askName;
+export const evenGame = () => {
+  const NumbersRange = {
+    MIN: 0,
+    MAX: 100,
+  };
+  const Answer = {
+    EVEN: 'yes',
+    UNEVEN: 'no',
+  };
+  const RIGHT_ANSWERS_FOR_WIN = 3;
+
+  const getRandomInt = () => Math.floor(Math.random() * NumbersRange.MAX) + NumbersRange.MIN;
+
+  console.log(`Answer "${Answer.EVEN}" if number even otherwise answer "${Answer.UNEVEN}".`);
+  console.log();
+
+  const actualName = askName();
+  const showWrongMessage = (answer, rightAnswer) => {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+    console.log(`Let's try again, ${actualName}!`);
+  };
+
+  let randomNumber;
+  let answer;
+  let rightAnswer;
+
+  for (let i = 0; i < RIGHT_ANSWERS_FOR_WIN; i += 1) {
+    randomNumber = getRandomInt();
+    rightAnswer = randomNumber % 2 === 0 ? Answer.EVEN : Answer.UNEVEN;
+    console.log(`Question: ${randomNumber}`);
+    answer = readlineSync.question('Your answer: ');
+    if (answer !== rightAnswer) {
+      showWrongMessage(answer, rightAnswer);
+      return;
+    }
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${actualName}!`);
+};
