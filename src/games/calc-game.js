028e5885dfca76d313c9af6529aa7c10a724685d
Герман <stgoge@ya.ru>
2018-08-02 10:09:48 +0700

    operations added

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -4,6 +4,20 @@ const NumbersRange = {
   MIN: 0,
   MAX: 100,
 };
+const Operation = [
+  {
+    SYMBOL: '+',
+    CALC: (a, b) => a + b,
+  },
+  {
+    SYMBOL: '-',
+    CALC: (a, b) => a - b,
+  },
+  {
+    SYMBOL: '*',
+    CALC: (a, b) => a * b,
+  },
+];
 
 const showRules = () => {
   console.log('What is the result of the expression?');
@@ -13,9 +27,10 @@ const showRules = () => {
 const generateQuestion = () => {
   const firstRandomNumber = getRandomInt(NumbersRange.MIN, NumbersRange.MAX);
   const secondRandomNumber = getRandomInt(NumbersRange.MIN, NumbersRange.MAX);
+  const operationNumber = getRandomInt(0, Operation.length - 1);
   return {
-    question: `${firstRandomNumber} + ${secondRandomNumber}`,
-    answer: `${firstRandomNumber + secondRandomNumber}`,
+    question: `${firstRandomNumber} ${Operation[operationNumber].SYMBOL} ${secondRandomNumber}`,
+    answer: `${Operation[operationNumber].CALC(firstRandomNumber, secondRandomNumber)}`,
   };
 };
 
