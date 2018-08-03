ffa5f3cedfa29861280a97cec12b8f9c21213613
Герман <stgoge@ya.ru>
2018-08-03 15:24:48 +0700

    step_9

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install: 
 	npm install 
 start:
-	npm run babel-node -- src/bin/brain-progression.js
+	npm run babel-node -- src/bin/brain-prime.js
 publish:
 	npm publish
 lint:
---
+++ src/games/prime-game.js
@@ -0,0 +1,23 @@
+import getRandomint from '../util';
+import gameProcess from '..';
+
+const minNumber = 0;
+const maxNumber = 100;
+const answerPrime = 'yes';
+const answerComposite = 'no';
+const description = `Answer "${answerPrime}" if number is prime otherwise answer "${answerComposite}".`;
+
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
+
+const generateAnswerAndQuestion = () => {
+  const question = getRandomint(minNumber, maxNumber);
+  const answer = isPrime(question) ? answerPrime : answerComposite;
+  return { question, answer };
+};
+
+export default () => gameProcess({ description, generateAnswerAndQuestion });
