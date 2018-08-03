48717b56787d067c62fa785a3f879a8c455ca5dd
Герман <stgoge@ya.ru>
2018-08-03 14:20:37 +0700

    step_8

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install: 
 	npm install 
 start:
-	npm run babel-node -- src/bin/brain-gcd.js
+	npm run babel-node -- src/bin/brain-progression.js
 publish:
 	npm publish
 lint:
---
+++ src/games/progression-game.js
@@ -0,0 +1,37 @@
+import getRandomint from '../util';
+import gameProcess from '..';
+
+const progressionLenght = 10;
+const progressionStartsMin = 50;
+const progressionStartsMax = 150;
+const progressionStepMin = -10;
+const progressionStepMax = 10;
+const description = 'What number is missing in this progression?';
+
+const getProgression = () => {
+  const start = getRandomint(progressionStartsMin, progressionStartsMax);
+  const step = getRandomint(progressionStepMin, progressionStepMax);
+  let current = start;
+  const progression = [current];
+  for (let i = 1; i < progressionLenght; i += 1) {
+    current += step;
+    progression.push(current);
+  }
+  return progression;
+};
+
+const getQuestionString = (arr, num) => {
+  const newArr = arr.slice();
+  newArr[num] = '..';
+  return newArr.reduce((el, acc) => `${el} ${acc}`, '');
+};
+
+const generateAnswerAndQuestion = () => {
+  const arr = getProgression();
+  const missingNumber = getRandomint(0, progressionLenght);
+  const question = getQuestionString(arr, missingNumber);
+  const answer = `${arr[missingNumber]}`;
+  return { question, answer };
+};
+
+export default () => gameProcess({ description, generateAnswerAndQuestion });
